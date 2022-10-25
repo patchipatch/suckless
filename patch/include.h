8f93434b31298fdb109dc136379910aeb3e08737
patchi <vtruongg@disroot.org>
2022-10-25 19:18:24 -0400

    initial commit

---
+++ patch/include.h
@@ -0,0 +1,33 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+#include "bar.h"
+
+#include "bar_alpha.h"
+#include "bar_ltsymbol.h"
+#include "bar_status.h"
+#include "bar_status2d.h"
+#include "bar_tags.h"
+#include "bar_wintitle.h"
+
+/* Other patches */
+#include "attachx.h"
+#include "cyclelayouts.h"
+#include "restartsig.h"
+#include "scratchpad.h"
+#include "seamless_restart.h"
+#include "sticky.h"
+#include "swallow.h"
+#include "tagothermonitor.h"
+#include "togglefullscreen.h"
+#include "vanitygaps.h"
+/* Layouts */
+#include "layout_bstack.h"
+#include "layout_bstackhoriz.h"
+#include "layout_centeredmaster.h"
+#include "layout_centeredfloatingmaster.h"
+#include "layout_deck.h"
+#include "layout_fibonacci.h"
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
